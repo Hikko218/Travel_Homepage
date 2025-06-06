d3838da7e2deef8e73231d47805e44f861d51bf6
Hikko218 <heikor93@outlook.de>
2025-06-06 17:42:57 +0200

    added commit popup/updated footer

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -7,3 +7,13 @@ function showSection(sectionId) {
   const active = document.getElementById(sectionId);
   if (active) active.style.display = "block";
 }
+
+document.getElementById("contact_form").addEventListener("submit", function (e) {
+  e.preventDefault(); // verhindert echtes Absenden
+  document.getElementById("popup").style.display = "block";
+});
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+  document.getElementById("contact_form").reset();
+}
