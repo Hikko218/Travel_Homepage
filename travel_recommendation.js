1b98b988e6d6603cc75be9226a55acb934ab6a93
Hikko218 <heikor93@outlook.de>
2025-06-06 19:17:34 +0200

    working data fetch

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -7,7 +7,7 @@ function showSection(sectionId) {
   const active = document.getElementById(sectionId);
   if (active) active.style.display = "block";
 }
-
+// contact form popup
 document.getElementById("contact_form").addEventListener("submit", function (e) {
   e.preventDefault(); // verhindert echtes Absenden
   document.getElementById("popup").style.display = "block";
@@ -17,3 +17,41 @@ function closePopup() {
   document.getElementById("popup").style.display = "none";
   document.getElementById("contact_form").reset();
 }
+
+// Fetching travel data from an API
+
+const travel_recommendation_api = "travel_recommendation_api.json"
+
+search_btn.addEventListener('click', searchdestination);
+
+ 
+function searchdestination() {
+    const searchInput = document.getElementById("search_btn").value.toLowerCase();
+    
+    
+ 
+
+        fetch(travel_recommendation_api)
+        .then(response => response.json())
+         .then(data => {
+            const country = data.countries.find(item => item.name.toLowerCase() === searchInput);
+    
+        
+
+        if (country) {
+            const name = cities.name.join(', ');
+            const imageurl = cities.imageurl.join(', ');
+            const description = cities.description.join(', ');
+
+        } 
+        else {
+            console.log('Condition not found.');
+        }
+        
+
+        })
+
+
+        
+
+    }
